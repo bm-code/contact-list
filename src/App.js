2fca54f65ee2792540e10181bc9af62db7ff0134
Beni Morales <i.benimorales@gmail.com>
2022-03-27 13:49:56 +0200

    Añadido contactos de ejemplo

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import { useState } from "react";
 function App() {
 
   const contactos = [
-    { nombre: 'Beni', apellidos: 'Morales Silva', direccion: 'Avenida de la Ermita', ciudad: 'Sevilla', codigoPostal: 41808, telefono: 678678678 },
-    { nombre: 'Pepe', apellidos: 'Garrido Gutierrez', direccion: 'Calle de pepe', ciudad: 'Cádiz', codigoPostal: 42808, telefono: 654654654 },
-    { nombre: 'María', apellidos: 'López García', direccion: 'Calle de María', ciudad: 'Málaga', codigoPostal: 29006, telefono: 612612612 }
+    { nombre: 'Beni', apellidos: 'Morales', direccion: 'Dirección de Beni', ciudad: 'Sevilla', codigoPostal: 40400, telefono: 678678678 },
+    { nombre: 'Pepe', apellidos: 'García', direccion: 'Dirección de Pepe', ciudad: 'Cádiz', codigoPostal: 40400, telefono: 654654654 },
+    { nombre: 'María', apellidos: 'López', direccion: 'Dirección de María', ciudad: 'Málaga', codigoPostal: 40400, telefono: 612612612 }
   ];
 
   const [contactosActual, setContactos] = useState(contactos);
